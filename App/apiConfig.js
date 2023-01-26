4e169cae2a93cb0fc7ac6ae23d3445c787f82ade
marlonsmithwilliams@4mzglobal.com <marlonsmithwilliams@4mzglobal.com>
2023-01-26 18:25:39 -0400

    configure application

--- App/apiConfig.js
+++ App/apiConfig.js
@@ -1,5 +1,5 @@
 // The current application coordinates were pre-registered in a B2C tenant.
 const apiConfig = {
     b2cScopes: ["https://4mzglobal4mzgo.onmicrosoft.com/5f66b0d3-c627-4af2-8c28-aa6d1fec1f95/4mz-members.read", "https://4mzglobal4mzgo.onmicrosoft.com/5f66b0d3-c627-4af2-8c28-aa6d1fec1f95/4mz-members.write"],
-    webApi: "http://localhost:5000/hello"
+    webApi: "https://club.4mzglobal.com/members"
   };
